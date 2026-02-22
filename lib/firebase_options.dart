93e83781adb4da0e5077b8dcd83cc16efcf53a4e
CreamojAI <creamoj@gmail.com>
2026-02-22 20:28:34 +0100

    Add Firebase web configuration for Flutter web support

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -58,6 +55,15 @@ class DefaultFirebaseOptions {
     messagingSenderId: '793691819503',
     projectId: 'loop-rider-prototype',
     storageBucket: 'loop-rider-prototype.firebasestorage.app',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDSp5lXMuyE9zfeNEEyCumQEvA77UFFVGA',
+    appId: '1:793691819503:web:dloop-web-config',
+    messagingSenderId: '793691819503',
+    projectId: 'loop-rider-prototype',
+    storageBucket: 'loop-rider-prototype.firebasestorage.app',
+    authDomain: 'loop-rider-prototype.firebaseapp.com',
   );
 
 }
